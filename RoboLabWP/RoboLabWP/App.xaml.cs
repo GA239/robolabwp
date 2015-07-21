3037dabfbfe7e3f81fb89bfde8ef47c6db0ecc01
popo <ag239@yandex.ru>
2015-07-21 20:23:05 +0300

    Рефакторинг:
    реструктуризация файлов проекта

--- RoboLabWP/RoboLabWP/App.xaml.cs
+++ RoboLabWP/RoboLabWP/App.xaml.cs
@@ -8,20 +8,15 @@ using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using RoboLabWP.Resources;
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
 
 using RoboLabWP.Model.UserData;
 
